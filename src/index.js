cce383ca746234c0a73954f22cfe181e2d2fa31e
Mostafa Amini Nasab <mostafa.amini.nasab@gmail.com>
2020-06-11 15:50:07 +0430

    move keys constant outside of calculator function

--- src/index.js
+++ src/index.js
@@ -1,22 +1,22 @@
-export default function Calculator () {
-    const keys = {
-        NUM_1: 1,
-        NUM_2: 2,
-        NUM_3: 3,
-        NUM_4: 4,
-        NUM_5: 5,
-        NUM_6: 6,
-        NUM_7: 7,
-        NUM_8: 8,
-        NUM_9: 9,
-        NUM_0: 0,
-        ADD: '+',
-        SUBTRACT: '-',
-        MULTIPLY: '×',
-        DIVIDE: '÷',
-        EQUALS: '='
-    };
+const keys = {
+    NUM_1: 1,
+    NUM_2: 2,
+    NUM_3: 3,
+    NUM_4: 4,
+    NUM_5: 5,
+    NUM_6: 6,
+    NUM_7: 7,
+    NUM_8: 8,
+    NUM_9: 9,
+    NUM_0: 0,
+    ADD: '+',
+    SUBTRACT: '-',
+    MULTIPLY: '×',
+    DIVIDE: '÷',
+    EQUALS: '='
+};
 
+export default function Calculator () {
     function isNumber (key) {
         const numberKeys = [
             keys.NUM_0,
